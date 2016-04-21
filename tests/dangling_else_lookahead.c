872e38bbae5178a415bc836b2fbdcba01cef0620
François Pottier <francois.pottier@inria.fr>
2016-04-21 14:51:23 +0200

    Reformatting.

--- tests/dangling_else_lookahead.c
+++ tests/dangling_else_lookahead.c
@@ -3,8 +3,8 @@ typedef int T;
 void f(void) {
   for(int T; ;)
     if(1);
-  // T must be resolved outside of the scope of the "for"
-  // statement, hence denotes a typedef name:
+  // T must be resolved outside of the scope of the
+  // "for" statement, hence denotes a typedef name:
   T x;
   x = 0;
 }
