a5672d3d3b379068d97c4693bdc85b58aae7dd06
François Pottier <francois.pottier@inria.fr>
2016-05-13 15:48:52 +0200

    Comments in [Options].

--- options.ml
+++ options.ml
@@ -10,13 +10,14 @@
 
 (* [c99_scoping] is [true] if using C99/C11 scoping rules, [false] if
    using ANSI scoping rules. *)
-(* Should be set to [true] for C99/C11 parsing, [false] for C89/C90
-   parsing. *)
+(* This flag influences only the ANSI-compatible parser. It should be
+   set to [true] for C99/C11 parsing, [false] for C89/C90 parsing. *)
 let c99_scoping = ref false
 
 (* [atomic_strict_syntax] is [true] if [_Atomic] followed by an
    opening parenthesis is always a type specifier. *)
-(* Should be set to [true] for strictly compliant C parsing, but
-   setting it to [false] only accepts more programs (and no program can
-   be wrongly parsed because of that). *)
+(* It should be set to [true] for strictly compliant C11 parsing.
+   Setting it to [false] only causes more programs to be accepted; no
+   program can be parsed incorrectly. *)
+(* This flag directly influences only the lexer. *)
 let atomic_strict_syntax = ref true
