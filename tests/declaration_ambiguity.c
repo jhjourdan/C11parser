d99f8d99fec990c0af4079d16e022846bf9172a4
Jacques-Henri Jourdan <jacques-henri.jourdan@normalesup.org>
2017-02-07 14:27:40 +0100

    Zero variableS.

--- tests/declaration_ambiguity.c
+++ tests/declaration_ambiguity.c
@@ -1,8 +1,8 @@
 // declaration_ambiguity.c
 typedef int T;
 void f (void) {
-  unsigned int;   // declares zero variable of type "unsigned int"
-  const T;        // declares zero variable of type "const T"
+  unsigned int;   // declares zero variables of type "unsigned int"
+  const T;        // declares zero variables of type "const T"
   T x;            // T is still visible as a typedef name
   unsigned T;     // declares a variable "T" of type "unsigned"
   T = 1;
