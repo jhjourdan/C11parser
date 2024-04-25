7560d4f46d72f9a5bca8fab966fbf28147f19e23
Jacques-Henri Jourdan <jacques-henri.jourdan@cnrs.fr>
2024-04-25 15:23:26 +0200

    Fix sizeof on function type in test. (Issue #24)

--- tests/function-decls.c
+++ tests/function-decls.c
@@ -2,5 +2,5 @@ void foo() {
   int X;
   X = sizeof(void (*(*)())());
   X = sizeof(int(*)(int, float, ...));
-  X = sizeof(void (*(int arga, void (*argb)(double Y)))(void* Z));
+  X = sizeof(void (*(*)(int arga, void (*argb)(double Y)))(void* Z));
 }
